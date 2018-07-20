72a64350793995d63875cb8a090a613bd6689e4a
David Elbert <elbert@jhu.edu>
2018-07-20 15:46:16 +0000

    initial push

---
+++ NanoHUB_remote/mysecrets.py
@@ -0,0 +1,7 @@
+auth_data = {
+    'client_id': '47c5487f881d94ca9158de88bfd40ad3',       # Get this when you register a web app
+    'client_secret': '930ef2b6635e04b36a285269743375ca700eaf4b',   # Get this when you register a web app
+    'grant_type': 'password',
+    'username': 'dcelbert',        # Get this when you create a nanoHUB account
+    'password': 'CrHllva0F3yZ'         # Get this when you create a nanoHUB account
+ }
